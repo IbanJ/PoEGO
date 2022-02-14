fbaad9c95bb3d8c5bd623c8f652eb5ace9ad9d47
IbanJ <iban6698@hotmail.com>
2022-02-14 04:02:50 +0100

    Fragment navigation done and login halfway.

--- app/src/main/java/com/example/poego/LoginActivity.kt
+++ app/src/main/java/com/example/poego/LoginActivity.kt
@@ -1,9 +1,13 @@
 package com.example.poego
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.widget.EditText
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.firestore.FirebaseFirestore
 import java.lang.ref.Reference
 
@@ -23,7 +27,66 @@ class LoginActivity : AppCompatActivity() {
         auth = FirebaseAuth.getInstance()
         database = FirebaseFirestore.getInstance()
 
+    }
 
+    private fun register () {
+        createNewAccount()
+    }
 
+    private fun createNewAccount() {
+        val email:String = etEmail.text.toString()
+        val password:String = etEmail.text.toString()
+
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password))
+        {
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){
+
+                    if(it.isComplete){
+                        Toast.makeText(this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show()
+                        navigateToMain()
+
+                    }
+                    else{
+                        Toast.makeText(this, "Error al crear usuario", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+        else{
+            Toast.makeText(this, "Rellene los dos campos de texto", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+
+    private fun login () {
+        loginAccount()
+    }
+
+    private fun loginAccount () {
+        val email:String = etEmail.text.toString()
+        val password:String = etEmail.text.toString()
+
+        if(!TextUtils.isEmpty(email) && !TextUtils.isEmpty(password))
+        {
+            auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this){
+
+                    if(it.isComplete){
+                        Toast.makeText(this, "Usuario identificado correctamente", Toast.LENGTH_SHORT).show()
+                        navigateToMain()
+
+                    }
+                    else{
+                        Toast.makeText(this, "Error al identificar usuario", Toast.LENGTH_SHORT).show()
+                    }
+                }
+        }
+        else{
+            Toast.makeText(this, "Rellene los dos campos de texto", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun navigateToMain() {
+        Intent(this, MainActivity::class.java)
     }
 }
--- app/src/main/java/com/example/poego/MainActivity.kt
+++ app/src/main/java/com/example/poego/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.poego
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomnavigation : BottomNavigationView
+    private lateinit var navListener : BottomNavigationView.OnNavigationItemSelectedListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomnavigation = findViewById(R.id.bottom_navigation)
+        bottomnavigation.setOnNavigationItemSelectedListener(
+            fun(item: MenuItem): Boolean {
+
+                lateinit var selectedFragment : Fragment
+                when (item.itemId) {
+                    R.id.nav_beguinners -> {
+                        selectedFragment = BeginnerGuide()
+                    }
+                    R.id.vendors_recipe -> {
+                        selectedFragment = VendorRecipes()
+                    }
+                    R.id.nav_user_settings -> {
+                        selectedFragment = SettingFragment()
+                    }
+                }
+                supportFragmentManager.beginTransaction().replace(R.id.fragment_container, selectedFragment).commit()
+                return true
+            })
+        supportFragmentManager.beginTransaction().replace(R.id.fragment_container, BeginnerGuide()).commit()
+
     }
 }
